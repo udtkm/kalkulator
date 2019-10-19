ac523178e723b2077e9a7b29922134a3859a4418
udtkm <erykwolff@gmail.com>
2019-10-19 09:08:46 +0200

    Add menu to my app

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("0. Wyjście");
+                Console.WriteLine("1.Dodawanie");
+                Console.WriteLine("2.Odejmowanie");
+                int menuOpt = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+    }
+}
